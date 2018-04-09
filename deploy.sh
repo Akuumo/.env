729c456d54d0e370069ce95db9fa9c02d7ac0a0c
Akuumo <mesnil.sam.pro@hotmail.com>
2018-04-09 12:08:42 +0200

    matin 09/04

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-ln -s .bashrc ~/.env/files/bashrc
-ln -s .ssh ~/.env/files/ssh
-ln -s .vim ~/.env/files/vim
+files=("bashrc" ".ssh" ".vim") 
+
+for file in "${files[@]}"
+do
+	if [ -e $file ]
+	then
+		printf "%s déplacé dans backup\n" "$file"
+		#mv $file ~/.env/backup/
+	else
+		printf "%s n'existe pas\n" "$file"
+	fi
+done
+
+#ln -s ~/.env/files/bashrc ~/.bashrc 
+#ln -s ~/.env/files/ssh ~/.ssh 
+#ln -s ~/.env/files/vim ~/.vim 
